b6724465ea5b2f3f02a08faa3d63d9098ad3fe26
Neeraj <eng20cs0301.neeraj@gmail.com>
2025-03-31 05:25:16 +0530

    2nd commit

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './app.css'; // Global styles
+// import './App.css'; // Global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
